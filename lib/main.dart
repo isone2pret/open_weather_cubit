397e0e006d0ff8c4fa470bfe17decd36f161c1fb
Wawan <mochwawankurnia@gmail.com>
2022-12-28 11:32:04 +0700

    Firs
    First

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:open_weather_cubit/cubits/temp_settings/temp_settings_cubit.dart';
+import 'package:open_weather_cubit/cubits/weather/cubit/weather_cubit.dart';
+import 'package:open_weather_cubit/pages/home_page.dart';
+import 'package:open_weather_cubit/repositories/weather_repository.dart';
+import 'package:open_weather_cubit/services/weather_api_services.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+          weatherApiServices: WeatherApiServices(httpClient: http.Client())),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<WeatherCubit>(
+              create: (context) => WeatherCubit(
+                  weatherRepository: context.read<WeatherRepository>())),
+          BlocProvider<TempSettingsCubit>(
+              create: (context) => TempSettingsCubit()),
+        ],
+        child: MaterialApp(
+            title: 'Weather App',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const HomePage()),
+      ),
+    );
+  }
+}
