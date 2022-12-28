571a295b99a772064060910e622aab8425e5e881
Wawan <mochwawankurnia@gmail.com>
2022-12-28 16:21:36 +0700

    perbaiikan

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,6 @@ import 'package:recase/recase.dart';
 import '../constants/constans.dart';
 import '../cubits/temp_settings/temp_settings_cubit.dart';
 
-
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -74,136 +72,136 @@ class _HomePageState extends State<HomePage> {
       body: _showWeather(),
     );
   }
-}
 
-String showTemperature(double temperature) {
-  
-  final tempUnit = context.watch<TempSettingsCubit>().state.tempUnit;
-  if (tempUnit == TempUnit.fahrenheit) {
-    return ((temperature * 9/5)+32).toStringAsFixed(2) + '℉';
+  String showTemperature(double temperature) {
+    final tempUnit = context.watch<TempSettingsCubit>().state.tempUnit;
+    if (tempUnit == TempUnit.fahrenheit) {
+      return ((temperature * 9 / 5) + 32).toStringAsFixed(2) + '℉';
+    }
+    return temperature.toStringAsFixed(2) + '℃';
   }
-  return temperature.toStringAsFixed(2) + '℃';
-}
 
-Widget showIcon(String icon) {
-  return FadeInImage.assetNetwork(
-    placeholder: 'assets/images/loading.gif',
-    image: 'http://$kIconHost/img/wn/$icon@4x.png',
-    width: 96,
-    height: 96,
-  );
-}
+  Widget showIcon(String icon) {
+    return FadeInImage.assetNetwork(
+      placeholder: 'assets/images/loading.gif',
+      image: 'http://$kIconHost/img/wn/$icon@4x.png',
+      width: 96,
+      height: 96,
+    );
+  }
 
-Widget formatText(String description) {
-  final formattedString = description.titleCase;
-  return Text(
-    formattedString,
-    style: const TextStyle(fontSize: 24),
-    textAlign: TextAlign.center,
-  );
-}
+  Widget formatText(String description) {
+    final formattedString = description.titleCase;
+    return Text(
+      formattedString,
+      style: const TextStyle(fontSize: 24),
+      textAlign: TextAlign.center,
+    );
+  }
 
-Widget _showWeather() {
-  return BlocConsumer<WeatherCubit, WeatherState>(
-    listener: (context, state) {
-      if (state.status == WeatherStatus.error) {
-        errorDialog(context, state.error.errMsg);
-      }
-    },
-    builder: (context, state) {
-      if (state.status == WeatherStatus.initial) {
-        return const Center(
-          child: Text(
-            "Select a city",
-            style: TextStyle(fontSize: 20),
-          ),
+  Widget _showWeather() {
+    return BlocConsumer<WeatherCubit, WeatherState>(
+      listener: (context, state) {
+        if (state.status == WeatherStatus.error) {
+          errorDialog(context, state.error.errMsg);
+        }
+      },
+      builder: (context, state) {
+        if (state.status == WeatherStatus.initial) {
+          return const Center(
+            child: Text(
+              "Select a city",
+              style: TextStyle(fontSize: 20),
+            ),
+          );
+        }
+
+        if (state.status == WeatherStatus.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (state.status == WeatherStatus.error && state.weather.name == '') {
+          return const Center(
+            child: Text(
+              "Select a city",
+              style: TextStyle(fontSize: 20),
+            ),
+          );
+        }
+
+        return ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Text(
+              state.weather.name,
+              textAlign: TextAlign.center,
+              style:
+                  const TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                    TimeOfDay.fromDateTime(state.weather.lastUpdated)
+                        .format(context),
+                    style: const TextStyle(fontSize: 18)),
+                const SizedBox(height: 10),
+                Text(
+                  '(${state.weather.country})',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+            const SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  showTemperature(state.weather.temp),
+                  style: const TextStyle(
+                      fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      showTemperature(state.weather.tempMax),
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      showTemperature(state.weather.tempMin),
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              children: [
+                Spacer(),
+                showIcon(state.weather.icon),
+                Expanded(child: formatText(state.weather.description)),
+                Spacer()
+              ],
+            )
+          ],
         );
-      }
-
-      if (state.status == WeatherStatus.loading) {
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-
-      if (state.status == WeatherStatus.error && state.weather.name == '') {
-        return const Center(
-          child: Text(
-            "Select a city",
-            style: TextStyle(fontSize: 20),
-          ),
-        );
-      }
-
-      return ListView(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 6,
-          ),
-          Text(
-            state.weather.name,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                  TimeOfDay.fromDateTime(state.weather.lastUpdated)
-                      .format(context),
-                  style: const TextStyle(fontSize: 18)),
-              const SizedBox(height: 10),
-              Text(
-                '(${state.weather.country})',
-                style: const TextStyle(fontSize: 18),
-              ),
-            ],
-          ),
-          const SizedBox(height: 60),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                showTemperature(state.weather.temp),
-                style: const TextStyle(
-                    fontSize: 30.0, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Column(
-                children: [
-                  Text(
-                    showTemperature(state.weather.tempMax),
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Text(
-                    showTemperature(state.weather.tempMin),
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                ],
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 40,
-          ),
-          Row(
-            children: [
-              Spacer(),
-              showIcon(state.weather.icon),
-              Expanded(child: formatText(state.weather.description)),
-              Spacer()
-            ],
-          )
-        ],
-      );
-    },
-  );
+      },
+    );
+  }
 }
